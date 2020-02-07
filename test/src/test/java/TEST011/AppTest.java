2c8a9c23edccd325c9a3619fd477b31b86b156a0
sangjunsong <34359833+sangjunsong@users.noreply.github.com>
2020-02-07 11:19:36 +0800

    Initial commit

---
+++ test/src/test/java/TEST011/AppTest.java
@@ -0,0 +1,20 @@
+package TEST011;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
